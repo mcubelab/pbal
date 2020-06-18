a012ad8e0d6771e757175942be288f4424bd39f5
taylorott <orion.thomas.taylor@gmail.com>
2020-06-18 16:23:38 -0700

    Added Projection Step, Visualization, Comments
    Added a projection method that projects the current set of generalized coordinates onto the constraint manifold. Also added further visualization.

--- ControlCode/DummySimScript02.m
+++ ControlCode/DummySimScript02.m
@@ -1,7 +1,14 @@
 %this script tests basic functionality of the MyPolygon and
 %PolygonRigidBody classes
 
-function res=DummySimScript01()
+function res=DummySimScript02()
+
+VisualizationTest01()
+
+end
+
+%Just visualizing a polygon using the simulation framework
+function res=VisualizationTest01()
 
 fig1=figure(1);
 hold on
@@ -11,116 +18,54 @@ axis equal
 axis([-l,l,-l,l])
 
 
+plist1=[0 , 10, 10;  ...
+       0 , -5,  5];
+r_cm1=[4;0];
+   
+plist2=[0 ,0 , 5, 5;  ...
+       -5, 5 , 5,  -5];
+   
+r_cm2=[2.5;0];
+
 M=10;
 I=100;
 
-plist=[0 , 10, 10;  ...
-       0 , -5,  5];
-   
-r_cm=[1000;0];
+test_rigid_body1=PolygonRigidBody(plist1, r_cm1, M, I);
+test_rigid_body2=PolygonRigidBody(plist2, r_cm2, M, I);
 
-test_rigid_body=PolygonRigidBody(plist, r_cm, M, I);
+rand_pos1=l*(rand(2,1)-.5);
+rand_theta1=rand(1)*2*pi;
 
+rand_pos2=l*(rand(2,1)-.5);
+rand_theta2=rand(1)*2*pi;
 
-test_rigid_body.initialize_visualization();
-plot(0,0,'ro','markerfacecolor','r');
-% 
-% for theta=0:.01:2*pi
-%    test_rigid_body.set_p_and_theta([0;0],theta);
-%    test_rigid_body.update_visualization();
-%    drawnow;
-% end
-% 
-% for theta=0:.01:2*pi
-%    test_rigid_body.set_p_and_theta([5*sin(theta);0],0);
-%    test_rigid_body.update_visualization();
-%    drawnow;
-% end
-% 
-% for theta=0:.01:2*pi
-%    test_rigid_body.set_p_and_theta([0;5*sin(theta)],0);
-%    test_rigid_body.update_visualization();
-%    drawnow;
-% end
+test_rigid_body1.set_p_and_theta(rand_pos1,rand_theta1);
+test_rigid_body2.set_p_and_theta(rand_pos2,rand_theta2);
 
+test_constraint1=PolygonConstraint();
 
-dt=0.1;
+test_constraint1.StickingContactOneBody(test_rigid_body1,[10;0],[0;0]);
 
-theta=-pi/2+.3;
+test_constraint2=PolygonConstraint();
 
-pivot=[0;0];
+test_constraint2.StickingContactTwoBodies(test_rigid_body1,plist1(:,2),test_rigid_body2,plist2(:,3));
 
-pout_temp=PolygonMath.rigid_body_position([0;0],theta,pivot);
+myEnvironment=SimulationEnvironment();
 
-test_rigid_body.set_p_and_theta(-pout_temp,theta)
+myEnvironment.addRigidBody(test_rigid_body1);
+myEnvironment.addRigidBody(test_rigid_body2);
+myEnvironment.addConstraint(test_constraint1);
+myEnvironment.addConstraint(test_constraint2);
 
+myEnvironment.initialize_visualization();
 
-[x,y,Dx,Dy,Hx,Hy]=PolygonMath.rigid_body_position_derivatives(test_rigid_body.LocalPolygon.position,theta,pivot);
-
-omega=0;
-dgeneralized=[Dx;Dy;[0,0,1]]\[0;0;omega];
-
-% generalized=[position;theta];
-
-
-test_rigid_body.set_v_and_omega(dgeneralized(1:2),dgeneralized(3));
-
-g=-100;
-for count=1:10000
-
-    [p,theta]=test_rigid_body.get_p_and_theta();
-    [v,omega]=test_rigid_body.get_v_and_omega();
-    
-    generalized=[p;theta];
-    dgeneralized=[v;omega];
-    
-    [x,y,Dx,Dy,Hx,Hy]=PolygonMath.rigid_body_position_derivatives(test_rigid_body.LocalPolygon.position,theta,pivot);
-    
-    [x_cm,y_cm,Dx_cm,Dy_cm,Hx_cm,Hy_cm]=PolygonMath.rigid_body_position_derivatives(generalized(1:2),generalized(3),r_cm);
- 
-%     Q1=M*Dx_cm'*Dx_cm+M*Dy_cm'*Dy_cm+I*[0,0,0;0,0,0;0,0,1]
-%     Q2=M*(Hx_cm*dgeneralized)'*Dx_cm'+M*(Hy_cm*dgeneralized)'*Dy_cm';
-%     Q2=-(Q2+Q2')*dgeneralized;
-
-
-    
-    [M,V] = test_rigid_body.LagrangeVM();
-    
-    BigMat=[[M,-Dx',-Dy'];[Dx,0,0];[Dy,0,0]];
-    BigVec=[g*Dy_cm'+V;-dgeneralized'*Hx*dgeneralized;-dgeneralized'*Hy*dgeneralized];
-    Accel=BigMat\BigVec;
-    Accel_generalized=Accel(1:3);
-
-   dgeneralized=dgeneralized+Accel_generalized*dt;
-   generalized=generalized+dgeneralized*dt;
-
- 
-   for n=1:10
-       [x,y,Dx,Dy,Hx,Hy]=PolygonMath.rigid_body_position_derivatives([generalized(1);generalized(2)],generalized(3),pivot);
-       generalized=generalized-[Dx;Dy]\[x;y];
-   end
-
-   [x,y,Dx,Dy,Hx,Hy]=PolygonMath.rigid_body_position_derivatives([generalized(1);generalized(2)],generalized(3),pivot);
-    
-   temp_mat=[[eye(3),-Dx',-Dy'];[Dx,0,0];[Dy,0,0]];
-   temp_vec=[0;0;0;-Dx*dgeneralized;-Dy*dgeneralized];
-   
-   temp_generalized=temp_mat\temp_vec;
-   
-   
-   dgeneralized=dgeneralized+temp_generalized(1:3);
-   
-   test_rigid_body.set_p_and_theta(generalized(1:2),generalized(3));
-   test_rigid_body.set_v_and_omega(dgeneralized(1:2),dgeneralized(3));
-   test_rigid_body.update_visualization();
-   drawnow;
-   
-   
-   
+myEnvironment.ConstraintProjection();
+drawnow;
+pause(1)
+myEnvironment.update_visualization();
+drawnow;
 end
 
-
-
-
+function res=VisualizationTest02()
 
 end
