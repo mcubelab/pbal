f74d672023e1c2596c8b1572b92baeb8f7e756a6
neeld <nddoshi9@gmail.com>
2020-08-26 18:08:33 -0400

    working on intergating kf + mpc, have some bugs

--- MPCCode/TestKFplusMPC.m
+++ MPCCode/TestKFplusMPC.m
@@ -2,29 +2,27 @@
 clear; clc; close all;
 addpath('./Plants', './Solvers', '../ControlCode/')
 
-% velocity desired
-VD = 0; % radians per second
 
 % true parmeters
-x=-pi/2;
+x=pi-pi/2;
 dxdt=0;
 a=7;
 b=5;
-theta_0=pi/4;
+theta_0=0;
 x_c=1;
 y_c=-2;
 R=8;
 X=[x;dxdt;a;b;theta_0;x_c;y_c;R];
 
 % inital guess
-x_guess=-pi/2;
-dxdt_guess=0;
-a_guess=7;
-b_guess=5;
-theta_0_guess=pi/4;
-x_c_guess=1;
-y_c_guess=2;
-R_guess=8;
+x_guess= pi-pi/3;
+dxdt_guess=0.2;
+a_guess=7-1;
+b_guess=5+1;
+theta_0_guess=pi/6;
+x_c_guess=1-0.2;
+y_c_guess=-2+0.2;
+R_guess=8-1;
 X_guess=[x_guess;dxdt_guess;a_guess;b_guess;theta_0_guess;x_c_guess;y_c_guess;R_guess];
 
 % kalman pendulum plant
@@ -35,6 +33,7 @@ params_guess.t_m = params_guess.m * params_guess.g * params_guess.l; % torque li
 params_guess.b = 0.0;  % damping
 params_guess.mu = 0.3; % coefficient of friction
 p_guess = PendulumPlant01(params_guess);
+p_guess.setPivot(x_c_guess,y_c_guess);
 
 % true plant
 params.l = 1; % length
@@ -44,10 +43,11 @@ params.t_m = params.m * params.g * params.l; % torque limit on input
 params.b = 0.0;  % damping
 params.mu = 0.3; % coefficient of friction
 p = PendulumPlant01(params);
+p.setPivot(x_c,y_c);
 
 % mpc parameters
 mpc_params.Nmpc = 20;    % mpc horizon
-mpc_params.Ntraj = 50;   % trajectory length
+mpc_params.Ntraj = 100;  % trajectory length
 mpc_params.dt = 0.01;    % time-step
 mpc_params.QN = blkdiag(eye(p.nq), 0.1*eye(p.nv));
 mpc_params.Q = blkdiag(eye(p.nq), 0.1*eye(p.nv));
@@ -61,9 +61,9 @@ P=.1*eye(8);
 xk = [x_c; y_c; x; 0; 0; dxdt]; % true initial state
 xk_guess = [x_c_guess; y_c_guess; x_guess; 0; 0; dxdt_guess]; % guess initial state
 
-xg = [x_c; y_c; x; 0; 0; 0]; % goal state
+xg = [x_c; y_c; pi; 0; 0; 0]; % goal state
 ug = [0; p_guess.m * p_guess.g;
-    0.5 * p_guess.m * p_guess.g * p_guess.l*sin(x)];
+    0.5 * p_guess.m * p_guess.g * p_guess.l*sin(pi)];
 
 % nominal trajectory
 mpc_params.Xnom = repmat(xg, 1, mpc_params.Ntraj);
@@ -74,46 +74,57 @@ mpc_tv = TimeVaryingMPC(p, mpc_params);
 
 % plotting
 xvec = xk;
-Xguess_vec = X_guess;
+X_guessvec = X_guess;
 uvec = [];
 utruevec = [];
 cvec = [];
+Pvec = P;
 
-for k = 1:mpc_tv.Ntraj
+
+% xk and xkp1 are in the format used in the mpc: [xp, yp, tht, vx_p, vy_p,
+% omega];
+
+% X and X_guess is in the format [tht_c; omega; a; b; tht_0; xp; yp; R];  
+
+for k = 1:2000 %1:mpc_tv.Ntraj
     disp(k)
-    disp(X_guess'); 
+%     disp(X_guess');
     
     % compute control input
-    [dx_mpc, dU_mpc] = mpc_tv.run_mpc(k, xk_guess);
-    uk = mpc_tv.Unom(:, k) + dU_mpc(1:mpc_tv.nu);
+%     [dx_mpc, dU_mpc] = mpc_tv.run_mpc(k, xk);
+%     uk = mpc_tv.Unom(:, k) + dU_mpc(1:mpc_tv.nu);
+
+    uk = [0; 0; -0.3*(xk(6) - 3 * sin(mpc_tv.dt * k))]; 
     
     % advance true state
     [xkp1, uk_true] = p.dynamics_solve(xk, uk, mpc_tv.dt);
     
     % KF
-    Z = p.my_KalmannOutputNoPartials(X);    
-    [dXdt_guess,dPdt]= p_guess.extended_kalmann_update(Z,X_guess,uk,P,Q,R);
+    Z = p.my_KalmannOutputNoPartials(X);
+    [dXdt_guess,dPdt]= p_guess.extended_kalmann_update(Z,X_guess, ...
+        uk_true,P,Q,R);
     
     % advance guess x
     P=P+mpc_tv.dt*dPdt;
-    X_guess=X_guess+mpc_tv.dt*dXdt_guess;  
-    xk_guess = [X_guess(6); X_guess(7); X_guess(1) - X_guess(5); 
-        0; 0; X_guess(2)];
-
+    X_guess=X_guess+mpc_tv.dt*dXdt_guess;
+%     xk_guess = [X_guess(6); X_guess(7); X_guess(1) - X_guess(5);
+%         0; 0; X_guess(2)];
+    
     % re-build mpc w/ new parameters
-    mpc_tv = TimeVaryingMPC(p, mpc_params);
+%     mpc_tv = TimeVaryingMPC(p_guess, mpc_params);
     
     % store solution
     xvec = [xvec, xkp1];
     uvec = [uvec, uk];
     utruevec = [utruevec, uk_true];
     cvec = [cvec, p.equality_const(xk, uk)];
-    Xguess_vec = [Xguess_vec, X_guess];
+    X_guessvec = [X_guessvec, X_guess];
+    Pvec = [Pvec, P];
     
     % update true x
     xk = xkp1;
-    X(1) = xkp1(3);
-    X(2) = xkp1(6); 
+    X(1) = xkp1(3) + X(5);
+    X(2) = xkp1(6);
 end
 
 
@@ -129,14 +140,14 @@ disp(t(end) * mpc_tv.dt);
 % animation
 figure(1); clf;
 hold on; axis equal;
-xlim(x_c + [-2*p.l, 2*p.l])
-ylim(y_c + [-2*p.l, 2*p.l])
-ph = plot( [0; xvec(1, 1) + p.l * sin(xvec(3,1))], ...
-    [0; xvec(2, 1) - p.l * cos(xvec(3,1))]);
+xlim(x_c + [-1.5, 1.5]*p.l)
+ylim(y_c + [-1.5, 1.5]*p.l)
+ph = plot( [xvec(1, 1); xvec(1, 1) + p.l * sin(xvec(3,1))], ...
+    [ xvec(2, 1); xvec(2, 1) - p.l * cos(xvec(3,1))]);
 th = title(sprintf('time: %f', 0.0));
 for i = 1:numel(t)
-    set(ph, 'Xdata',  [0; xvec(1, i) + p.l * sin(xvec(3,i))], ...
-        'Ydata', [0; 2 * xvec(2, i) -  p.l * cos(xvec(3,i))])
+    set(ph, 'Xdata',  [ xvec(1, i); xvec(1, i) + p.l * sin(xvec(3,i))], ...
+        'Ydata', [ xvec(2, i); xvec(2, i) -  p.l * cos(xvec(3,i))])
     set(th, 'string', sprintf('time: %f', t(i)*mpc_tv.dt))
     pause(mpc_tv.dt)
 end
@@ -149,16 +160,42 @@ multiplier = [1, 1, (180/pi), 1, 1, (180/pi)];
 
 for k = 1:(p.nq + p.nv)
     subplot(2, 3, k); hold on;
-    if k == 3
-        plot(t, multiplier(k) * X_guess(1, :))
+    plot(t, multiplier(k)*xvec(k, :));
+    
+    if k == 1
+        shadedErrorBar(t, multiplier(k)*X_guessvec(6, :), ...
+            multiplier(k)*sqrt(Pvec(6, 6:8:end)))
     end
     
+    if k == 2
+        shadedErrorBar(t, multiplier(k)*X_guessvec(7, :), ...
+            multiplier(k)*sqrt(Pvec(7, 7:8:end)))
+    end
+    
+    if k == 3
+        shadedErrorBar(t, multiplier(k)*X_guessvec(1, :), ...
+            multiplier(k)*sqrt(Pvec(1, 1:8:end)))
+    end
     if k == 6
-        plot(t, multiplier(k) * X_guess(2, :))
-    end        
-    plot(t, multiplier(k)*xvec(k, :));
+        shadedErrorBar(t, multiplier(k)*X_guessvec(2, :), ...
+            multiplier(k)*sqrt(Pvec(2, 2:8:end)))
+    end
     title(titles{k})
 end
+
+figure(5); clf;
+subplot(3,1,1); hold on;
+shadedErrorBar(t, X_guessvec(3, :), sqrt(Pvec(3, 3:8:end)))
+yline(a)
+title('Gravity-ish term')
+subplot(3,1,2); hold on;
+shadedErrorBar(t, X_guessvec(4, :), sqrt(Pvec(4, 4:8:end)))
+yline(b)
+title('Mass-ish term')
+subplot(3,1,3); hold on;
+shadedErrorBar(t, X_guessvec(5, :), sqrt(Pvec(5, 5:8:end)))
+yline(theta_0)
+title('Theta_0')
 
 % input
 figure(3); clf;
