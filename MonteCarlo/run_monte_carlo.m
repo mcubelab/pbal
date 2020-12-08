3572327b9d0f0572e5119811c227d86d3d797668
neeld <nddoshi9@gmail.com>
2020-12-08 12:53:38 -0500

    working on monte-carlo sim

---
+++ MonteCarlo/run_monte_carlo.m
@@ -0,0 +1,9 @@
+clear; clc; close all; 
+addpath('../ControlCode/')
+
+
+% physical parameters (MORE INTUITIVE TO TUNE)
+% mass = 0.25; % kg
+% length = 0.1; % m
+% gravity = 10; % m/s^2
+% inertia = mass*length^2;
