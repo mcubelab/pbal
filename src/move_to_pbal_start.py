e05dba46da980173de3755d8aa4099dc49701879
neeld <nddoshi9@gmail.com>
2021-11-09 16:49:48 -0500

    fixing some more after switching to franka_interface_master

--- src/move_to_pbal_start.py
+++ src/move_to_pbal_start.py
@@ -1,7 +1,18 @@
 import rospy
 from franka_interface import ArmInterface
 
+pbal_start_pose = {
+        "panda_joint1": -0.12285389533116273,
+        "panda_joint2": 0.6495492118908847,
+        "panda_joint3": -0.03179291257694759,
+        "panda_joint4": -2.08736953684449,
+        "panda_joint5": 1.4410093290209771,
+        "panda_joint6": 1.5131599545882595,
+        "panda_joint7": 0.4397166739718772}
+
 if __name__ == '__main__':
     rospy.init_node("move_to_pbal_node")
     r = ArmInterface()
-    r.move_to_pbal_start()
+    r.set_joint_position_speed(speed=0.15)
+    r.move_to_joint_positions(positions=pbal_start_pose,
+        timeout=15.0) 
--- src/realsense_liveplot_test.py
+++ src/realsense_liveplot_test.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 import tf
 import tf.transformations as tfm
 from ros_helper import lookupTransform
@@ -46,7 +47,10 @@ def camera_info_callback(data):
     camera_info = data
 
 def load_shape_data(name_in):
-    f = open("/home/oneills/Documents/panda/base_ws/src/franka_ros_interface/franka_ros_controllers/scripts/models/shape_description/"+name_in+".json")
+    curr_dir = os.path.dirname(__file__)
+    fname = os.path.join(curr_dir, 'models', 'shape_description', name_in+".json")
+    f = open(fname)
+    # f = open("/home/oneills/Documents/panda/base_ws/src/franka_ros_interface/franka_ros_controllers/scripts/models/shape_description/"+name_in+".json")
     shape_data = json.load(f)
 
     vertex_array = np.array([shape_data["x_vert"],shape_data["y_vert"]])
