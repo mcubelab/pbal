8f4fdee588c822e7a3f0f8960ebde4a84fe00343
neeld <nddoshi9@gmail.com>
2021-12-10 14:24:54 -0500

    fixing data files

--- src/ControlandExecution/tests/compute_system_delay01.py
+++ src/ControlandExecution/tests/compute_system_delay01.py
@@ -25,7 +25,7 @@ def cos_wave(t, amplitude_in, period_in):
 
 if __name__ == '__main__':
 
-    with open('RobotDelayData16.txt', 'r') as f:
+    with open('RobotDelayData15.txt', 'r') as f:
         data_dict = json.loads(f.read())
 
     impedance_target_horizontal_pose_list = data_dict["impedance_target_horizontal_pose_list"]
