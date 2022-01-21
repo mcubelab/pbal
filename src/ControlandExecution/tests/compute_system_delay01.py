fixed system delay computation stuff

src/ControlandExecution/tests/compute_system_delay01.py
@@ -94,39 +94,49 @@ if __name__ == '__main__':
     amplitude_vert = np.sqrt(Coeff_Vec_vert[0]**2+Coeff_Vec_vert[1]**2)
     phase_vert = np.arctan2(Coeff_Vec_vert[1], Coeff_Vec_vert[0])*180.0/np.pi
 
-    fig, axs = plt.subplots(4, 1, figsize=(20, 10))
+    fig, axs = plt.subplots(4, 1, figsize=(16, 9))
+
+    # change font size for axes
+    for ax in axs.flatten():
+        for tick in ax.get_xticklabels():
+            tick.set_fontsize(18)
+        for tick in ax.get_yticklabels():
+            tick.set_fontsize(18)
+
     axs[0].plot(tlist, impedance_target_horizontal_pose_list,
-                marker='o', color='b', label='target')
-    axs[0].plot(tlist, measured_horizontal_pose_list,
-                marker='o', color='r', label='measured')
+                marker='.', color='b', label='target')
+    # axs[0].plot(tlist, measured_horizontal_pose_list,
+    #             marker='o', color='r', label='measured')
 
     axs[2].plot(tlist, impedance_target_vertical_pose_list,
-                marker='o', color='b', label='target')
-    axs[2].plot(tlist, measured_vertical_pose_list,
-                marker='o', color='r', label='measured')
+                marker='.', color='b', label='target')
+    # axs[2].plot(tlist, measured_vertical_pose_list,
+    #             marker='o', color='r', label='measured')
 
-    axs[3].set_xlabel('time (s)')
-    # axs[1].set_xlabel('time (s)')
+    axs[3].set_xlabel('Time (s)', fontsize=18)
+    # axs[1].set_xlabel('Time (s)')
 
-    axs[0].set_ylabel('position (m)')
-    axs[2].set_ylabel('position (m)')
+    axs[0].set_ylabel('Position (m)', fontsize=18)
+    axs[2].set_ylabel('Position (m)', fontsize=18)
+    axs[1].set_ylabel('Force (N)', fontsize=18)
+    axs[3].set_ylabel('Force (N)', fontsize=18)
 
-    axs[0].legend()
-    axs[1].legend()
+    # axs[0].legend()
+    # axs[1].legend()
 
-    axs[0].set_title('Horizontal Position')
-    axs[1].set_title('Horizontal Force')
+    # axs[0].set_title('Horizontal Position')
+    # axs[1].set_title('Horizontal Force')
 
-    axs[1].plot(tlist, measured_horiztonal_force_list, color='b')
-    axs[1].plot(tlist, Coeff_Vec_horz[2] + sine_wave(tlist -
-                delay_horz, amplitude_horz, period), color='r')
+    axs[1].plot(tlist, measured_horiztonal_force_list, color='k')
+    # axs[1].plot(tlist, Coeff_Vec_horz[2] + sine_wave(tlist -
+    #             delay_horz, amplitude_horz, period), color='r')
 
-    axs[3].plot(tlist, measured_vertical_force_list, color='b')
-    axs[3].plot(tlist, Coeff_Vec_vert[2] + cos_wave(tlist -
-                delay_vert, amplitude_vert, period), color='r')
+    axs[3].plot(tlist, measured_vertical_force_list, color='k')
+    # axs[3].plot(tlist, Coeff_Vec_vert[2] + cos_wave(tlist -
+    #             delay_vert, amplitude_vert, period), color='r')
 
-    axs[2].set_title('Vertical Position')
-    axs[3].set_title('Vertical Force')
+    # axs[2].set_title('Vertical Position')
+    # axs[3].set_title('Vertical Force')
 
     print("horizontal delay (s) = ", delay_horz)
     print("vertical delay (s) = ", delay_vert)

src/ControlandExecution/tests/compute_system_delay_batch01.py
@@ -7,6 +7,7 @@ import sys
 from typing import ForwardRef
 
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import itertools as it
 
@@ -87,7 +88,7 @@ def analyze_data_file(filename, plot_data=True):
     phase_vert = np.arctan2(Coeff_Vec_vert[1], Coeff_Vec_vert[0])*180.0/np.pi
     if plot_data:
 
-        fig, axs = plt.subplots(4, 1, figsize=(20, 10))
+        fig, axs = plt.subplots(4, 1, figsize=(16, 9))
         axs[0].plot(tlist, impedance_target_horizontal_pose_list,
                     marker='o', color='b', markersize=1, label='target')
         axs[0].plot(tlist, measured_horizontal_pose_list,
@@ -159,16 +160,23 @@ if __name__ == '__main__':
     # sort by filter params and plot
     fig, axs = plt.subplots(2, 2, figsize=(20, 10))
 
+    # change font size for axes
+    for ax in axs.flatten():
+        for tick in ax.get_xticklabels():
+            tick.set_fontsize(18)
+        for tick in ax.get_yticklabels():
+            tick.set_fontsize(18)
+
     # labels
-    axs[0, 0].set_title('Horizontal')
-    axs[0, 0].set_ylabel('latency (s)')
+    axs[0, 0].set_title('Horizontal', fontsize=18)
+    axs[0, 0].set_ylabel('Latency (s)', fontsize=18)
 
-    axs[1, 0].set_xlabel('freq (hz)')
-    axs[1, 0].set_ylabel('phase (deg)')
+    axs[1, 0].set_xlabel('Frequency (hz)', fontsize=18)
+    axs[1, 0].set_ylabel('Phase Delay (deg)', fontsize=18)
 
-    axs[0, 1].set_title('Vertical')
+    axs[0, 1].set_title('Vertical', fontsize=18)
 
-    axs[1, 1].set_xlabel('freq (hz)')
+    axs[1, 1].set_xlabel('Frequency (Hz)', fontsize=18)
 
     unique_filter_params = np.unique(np.array(filter_params_list))
     num_filter_params = len(unique_filter_params)
@@ -179,6 +187,11 @@ if __name__ == '__main__':
     phase_horz_list2 = []
     phase_vert_list2 = []
 
+    legend_dict = {unique_filter_params[0]: 'Lower Cutoff Freq',
+                   unique_filter_params[1]: 'Original Cutoff Freq',
+                   unique_filter_params[2]: 'Higher Cutoff Freq',
+                   unique_filter_params[3]: 'No Filter',
+                   }
     for filter_param in unique_filter_params:
         print(filter_param)
         mask = filter_params_list == filter_param
@@ -197,13 +210,13 @@ if __name__ == '__main__':
         sort_order = np.argsort(freq_list2[-1])
 
         axs[0, 0].semilogy(freq_list2[-1][sort_order], delay_horz_list2[-1][sort_order],
-                           linestyle='-', marker='o', label='filter params = '+str(filter_param))
+                           linestyle='-', marker='o', label=legend_dict[filter_param])
         axs[0, 1].semilogy(freq_list2[-1][sort_order], delay_vert_list2[-1][sort_order],
-                           linestyle='-', marker='o', label='filter params = '+str(filter_param))
+                           linestyle='-', marker='o', label=legend_dict[filter_param])
         axs[1, 0].plot(freq_list2[-1][sort_order], phase_horz_list2[-1][sort_order],
-                       linestyle='-', marker='o', label='filter params = '+str(filter_param))
+                       linestyle='-', marker='o', label=legend_dict[filter_param])
         axs[1, 1].plot(freq_list2[-1][sort_order], phase_vert_list2[-1][sort_order],
-                       linestyle='-', marker='o', label='filter params = '+str(filter_param))
+                       linestyle='-', marker='o', label=legend_dict[filter_param])
 
     # axs[1].scatter(freq_list, phase_horz_list, color='blue',
     #                marker='o', label='horizontal')
@@ -212,6 +225,6 @@ if __name__ == '__main__':
     # axs[0].set_title('Vertical')
     # axs[1].set_xlabel('freq (hz)')
     # axs[1].set_ylabel('phase (deg)')
-    axs[1, 1].legend()
+    axs[0, 0].legend(prop={'size': 14})
 
     plt.show()