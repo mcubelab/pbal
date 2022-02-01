8b32e1fe9ef1e066137ea58a841b48d34b0e2ef7
tayloro <orion.thomas.taylor@gmail.com>
2022-02-01 17:20:52 -0500

    starting work on new pivot estimator

---
+++ src/Estimation/test/test_GMM_reasoning01.py
@@ -0,0 +1,13 @@
+import json
+import pickle
+import numpy as np
+
+if __name__ == '__main__':
+	my_path = 'C:/Users/taylorott/Dropbox (MIT)/pbal_assets/Experiments\InitialEstimatorDataPlusQPDebug-Jan-2022/'
+	fname = '2022-01-21-17-16-17-experiment012-rectangle-no-mass.pickle'
+	
+
+	data_dict = pickle.load(open(my_path+fname, 'rb'))
+	print data_dict.keys()
+	print data_dict['ee_pose_in_world_from_franka_publisher'][0]
+	
