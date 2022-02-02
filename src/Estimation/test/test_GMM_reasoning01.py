f2d99b42b90b892cf0670dd93050dbcda9a19674
tayloro <orion.thomas.taylor@gmail.com>
2022-02-02 18:18:26 -0500

    blah blah

--- src/Estimation/test/test_GMM_reasoning01.py
+++ src/Estimation/test/test_GMM_reasoning01.py
@@ -1,6 +1,7 @@
 import json
 import pickle
 import numpy as np
+import scipy
 
 if __name__ == '__main__':
 	my_path = 'C:/Users/taylorott/Dropbox (MIT)/pbal_assets/Experiments\InitialEstimatorDataPlusQPDebug-Jan-2022/'
@@ -8,6 +9,6 @@ if __name__ == '__main__':
 	
 
 	data_dict = pickle.load(open(my_path+fname, 'rb'))
-	print data_dict.keys()
-	print data_dict['ee_pose_in_world_from_franka_publisher'][0]
+	#print data_dict.keys()
+	print data_dict['ee_pose_in_world_from_franka_publisher'][0]['msg']
 	
