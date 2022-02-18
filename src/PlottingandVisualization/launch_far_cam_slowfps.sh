540edf21a28fa266339ad3bceed8f7b4195dffea
anthonysimeonov <asimeono@mit.edu>
2022-02-18 11:15:31 -0500

    adding launch files for realsense cameras

---
+++ src/PlottingandVisualization/launch_far_cam_slowfps.sh
@@ -0,0 +1,1 @@
+roslaunch realsense2_camera rs_camera.launch camera:=far_cam serial_no:=811112060770 color_width:=1280 color_height:=720 color_fps:=15
---
+++ src/PlottingandVisualization/launch_near_cam.sh
@@ -0,0 +1,1 @@
+roslaunch realsense2_camera rs_camera.launch camera:=near_cam serial_no:=044322061246 color_width:=1280 color_height:=720 color_fps:=30
