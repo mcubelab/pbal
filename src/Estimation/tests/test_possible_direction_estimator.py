ce85b50d78be127160569ccfa3f97a98a976954d
taylorott <orion.thomas.taylor@gmail.com>
2023-01-18 22:19:01 -0500

    developing and testing feasible motion predictor

--- src/Estimation/tests/test_possible_direction_estimator.py
+++ src/Estimation/tests/test_possible_direction_estimator.py
@@ -64,12 +64,283 @@ class feasible_motion_estimator(object):
 
         self.friction_parameter_dict = system_state['friction_parameter_dict'] 
 
+        self.single_pivot_contact_index = self.contact_indices [0]
+
+
+    def check_motion_directions(self):
+
+        test = self.test_slide_right_ground_feasible_single_contact()
+
+        str_out = ''
+        if test:
+            # print('slide right ground')
+
+            str_out+='rg '
+
+        test = self.test_slide_left_ground_feasible_single_contact()
+
+        if test:
+            # print('slide left ground')
+
+            str_out+='lg '
+
+        test = self.test_slide_right_contact_feasible_single_contact()
+
+        if test:
+            # print('slide right contact')
+            str_out+='rc '
+
+        test = self.test_slide_left_contact_feasible_single_contact()
+
+        if test:
+            # print('slide left contact')
+            str_out+='lc '
+
+        lb,ub = self.compute_single_contact_pivot_range()
+
+        lb*=180/np.pi
+        ub*=180/np.pi
+
+        lb = round(lb)
+        ub = round(ub)
+
+        print(str_out)
+        print('min_theta (deg): ',lb,'max_theta (deg): ',ub)
+        print()
+
+    def test_slide_right_ground_feasible_single_contact(self):
+        inequality_constraints_enforced =  [self.friction_right_contact_constraint,
+                                            self.friction_left_contact_constraint,
+                                            self.torque_right_contact_constraint,
+                                            self.torque_left_contact_constraint,
+                                            self.normal_force_max_contact_constraint,
+                                            self.normal_force_min_contact_constraint,
+                                            self.normal_force_min_external_constraint,
+                                            self.friction_left_external_constraint]
+
+        equality_constraints_enforced = [self.torque_single_pivot_constant_angle_constraint]
+        inequality_constraints_violated = [self.friction_right_external_constraint]
+
+        can_move = self.check_single_contact_sliding_motion_feasibility(inequality_constraints_enforced,equality_constraints_enforced,inequality_constraints_violated,
+                                    enforcement_margin = 0., violation_margin = 0.)
+
+       
+        return can_move
+
+    def test_slide_left_ground_feasible_single_contact(self):
+        inequality_constraints_enforced =  [self.friction_right_contact_constraint,
+                                            self.friction_left_contact_constraint,
+                                            self.torque_right_contact_constraint,
+                                            self.torque_left_contact_constraint,
+                                            self.normal_force_max_contact_constraint,
+                                            self.normal_force_min_contact_constraint,
+                                            self.normal_force_min_external_constraint,
+                                            self.friction_right_external_constraint]
+
+        equality_constraints_enforced = [self.torque_single_pivot_constant_angle_constraint]
+        inequality_constraints_violated = [self.friction_left_external_constraint]
+
+        can_move = self.check_single_contact_sliding_motion_feasibility(inequality_constraints_enforced,equality_constraints_enforced,inequality_constraints_violated,
+                                    enforcement_margin = 0., violation_margin = 0.)
+
+       
+        return can_move
+
+    def test_slide_right_contact_feasible_single_contact(self):
+        inequality_constraints_enforced =  [self.friction_left_contact_constraint,
+                                            self.torque_right_contact_constraint,
+                                            self.torque_left_contact_constraint,
+                                            self.normal_force_max_contact_constraint,
+                                            self.normal_force_min_contact_constraint,
+                                            self.normal_force_min_external_constraint,
+                                            self.friction_left_external_constraint,
+                                            self.friction_right_external_constraint]
+
+        equality_constraints_enforced = [self.torque_single_pivot_constant_angle_constraint]
+        inequality_constraints_violated = [self.friction_right_contact_constraint]
+
+        can_move = self.check_single_contact_sliding_motion_feasibility(inequality_constraints_enforced,equality_constraints_enforced,inequality_constraints_violated,
+                                    enforcement_margin = 0., violation_margin = 0.)
+
+       
+        return can_move
+
+    def test_slide_left_contact_feasible_single_contact(self):
+        inequality_constraints_enforced =  [self.friction_right_contact_constraint,
+                                            self.torque_right_contact_constraint,
+                                            self.torque_left_contact_constraint,
+                                            self.normal_force_max_contact_constraint,
+                                            self.normal_force_min_contact_constraint,
+                                            self.normal_force_min_external_constraint,
+                                            self.friction_left_external_constraint,
+                                            self.friction_right_external_constraint]
+
+        equality_constraints_enforced = [self.torque_single_pivot_constant_angle_constraint]
+        inequality_constraints_violated = [self.friction_left_contact_constraint]
+
+        can_move = self.check_single_contact_sliding_motion_feasibility(inequality_constraints_enforced,equality_constraints_enforced,inequality_constraints_violated,
+                                    enforcement_margin = 0., violation_margin = 0.)
+
+       
+        return can_move
+
+    def compute_single_contact_pivot_range(self):
+        theta_max = np.pi/2
+        theta_min = self.theta_hand
+
+        while theta_max-theta_min>.01:
+            theta_test = (theta_max+theta_min)/2.0
+
+            test_val = self.single_contact_pivot_check_theta(theta_test)
+            
+
+            if test_val:
+                theta_min = theta_test
+            else:
+                theta_max = theta_test
+
+        upper_bound = theta_test
+
+        theta_max = self.theta_hand
+        theta_min = -np.pi/2
+
+        while theta_max-theta_min>.01:
+            theta_test = (theta_max+theta_min)/2.0
+
+            test_val = self.single_contact_pivot_check_theta(theta_test)
+
+            if test_val:
+                theta_max = theta_test
+            else:
+                theta_min = theta_test
+
+        lower_bound = theta_test
+
+        return lower_bound,upper_bound
+
+
+
+    def single_contact_pivot_check_theta(self,theta_test):
+
+        delta_theta = theta_test-self.theta_hand
+
+        rot_mat = np.array([[ np.cos(delta_theta),-np.sin(delta_theta), 0.], 
+                            [ np.sin(delta_theta), np.cos(delta_theta), 0.], 
+                            [                   0.,                 0., 1.]])
+
+        contact_constraints =  [self.friction_right_contact_constraint,
+                                self.friction_left_contact_constraint,
+                                self.torque_right_contact_constraint,
+                                self.torque_left_contact_constraint,
+                                self.normal_force_max_contact_constraint,
+                                self.normal_force_min_contact_constraint]
+
+        Aiq0, Biq0 = self.stack_constraints(contact_constraints,is_equality = False) 
+
+
+        equality_constraint = [self.torque_single_pivot_constant_angle_constraint]
+
+
+        Aiq1, Biq1 = self.stack_constraints(equality_constraint,is_equality = True)
+
+        #Aiq1[0] * delta_W + torque_term_increment == Biq1[0]
+        #Aiq1[1] * delta_W - torque_term_increment == Biq1[1]
+        #thus:
+        #Aiq1[0] * delta_W == Biq1[0] - torque_term_increment
+        #Aiq1[1] * delta_W == Biq1[1] + torque_term_increment
+
+        torque_term_increment = self.mgl_cos_theta_array*(np.cos(theta_test)-np.cos(self.theta_hand)) + self.mgl_sin_theta_array*(np.sin(theta_test)-np.sin(self.theta_hand)) 
+
+        Biq1[0]-=torque_term_increment
+        Biq1[1]+=torque_term_increment
+
+        external_constraints = [self.normal_force_min_external_constraint,
+                                self.friction_left_external_constraint,
+                                self.friction_right_external_constraint]
+
+        Aiq2, Biq2 = self.stack_constraints(external_constraints,is_equality = False) 
+
+        Aiq2 = np.dot(Aiq2,rot_mat)
+
+        A = np.vstack([Aiq0,Aiq1,Aiq2])
+        B = np.hstack([Biq0-np.dot(Aiq0,self.measured_ee_wrench),Biq1,Biq2-np.dot(Aiq2,self.measured_ee_wrench)])
+
+        C = np.array([1.0,0.0,0.0])
+
+        sol = solvers.lp(matrix(C),matrix(A),matrix(B))
+
+        if sol['x'] is not None:
+            return True
+
+        else:
+            return False 
+
+
+
+        
+
+
+
+
+    def check_single_contact_sliding_motion_feasibility(self,inequality_constraints_enforced,equality_constraints_enforced,inequality_constraints_violated,
+                                    enforcement_margin = .5, violation_margin = 1.0):
+        Aiq_enforced, Biq_enforced = self.stack_constraints(inequality_constraints_enforced,is_equality = False) 
+        Aeq_enforced, Beq_enforced = self.stack_constraints(equality_constraints_enforced,is_equality = True) 
+        Aiq_violated, Biq_violated = self.stack_constraints(inequality_constraints_violated,is_equality = False)
+
+        
+        A = np.vstack([np.array(Aiq_enforced),np.array(Aeq_enforced)])
+        q = Biq_enforced-np.dot(Aiq_enforced,self.measured_ee_wrench)-enforcement_margin
+        B = np.hstack([q, Beq_enforced])
+
+        B_check = Biq_violated-np.dot(Aiq_violated,self.measured_ee_wrench)
+
+
+
+        Amatrix = matrix(A)
+        Bmatrix = matrix(B)
+
+        for i in range(len(Aiq_violated)):
+            sol = solvers.lp(matrix(-Aiq_violated[i]),Amatrix,Bmatrix)
+
+
+            if sol['x'] is not None:
+                
+                if -sol['primal objective']>=B_check[i]+violation_margin:
+                    return True
+
+        return False
+
+
+
+    def stack_constraints(self,constraints_list, is_equality = False):
+        A, B = [], []
+        for constraint in constraints_list:
+            a, b = constraint()
+
+            if a.ndim == 1:
+                A.append(a)
+                B.append(b)
+
+                if is_equality:
+                    A.append(-a)
+                    B.append(-b)
+
+            else:
+                for i in range(len(b)):
+                    A.append(a[i])
+                    B.append(b[i])
+
+                    if is_equality:
+                        A.append(-a[i])
+                        B.append(-b[i])
+
+        return np.array(A), np.array(B)
 
     def friction_right_contact_constraint(self):
         ''' right (i.e., positive) boundary of friction cone '''
 
         aiq = np.array(self.friction_parameter_dict['acr'])
-        aiq = np.dot(aiq,self.C2R)
 
         biq = self.friction_parameter_dict['bcr']
 
@@ -79,7 +350,6 @@ class feasible_motion_estimator(object):
         ''' right (i.e., positive) boundary of friction cone '''
 
         aiq = np.array(self.friction_parameter_dict['acl'])
-        aiq = np.dot(aiq,self.C2R)
 
         biq = self.friction_parameter_dict['bcl']
 
@@ -90,7 +360,6 @@ class feasible_motion_estimator(object):
   
         aiq = np.array([-self.l_contact  / 2., 0., 1.])
         aiq/=np.abs(aiq[0])
-        aiq = np.dot(aiq,self.C2R)
 
         biq = 0.0
 
@@ -101,7 +370,6 @@ class feasible_motion_estimator(object):
 
         aiq = np.array([-self.l_contact  / 2., 0., -1.])
         aiq/=np.abs(aiq[0])
-        aiq = np.dot(aiq,self.C2R)
 
         biq = 0.0
 
@@ -111,7 +379,6 @@ class feasible_motion_estimator(object):
         ''' maximum applied normal force at contact constraint '''
 
         aiq = np.array([1., 0., 0.])
-        aiq = np.dot(aiq,self.C2R)
 
         biq = self.pivot_params['Nmax_contact']
 
@@ -121,7 +388,6 @@ class feasible_motion_estimator(object):
         ''' minimum applied normal force at contact constraint '''
 
         aiq = np.array([-1., 0., 0.])
-        aiq = np.dot(aiq,self.C2R)
 
         biq = -self.pivot_params['Nmin_contact']
 
@@ -131,6 +397,8 @@ class feasible_motion_estimator(object):
         ''' normal force at external contact must be above 0 '''
 
         aiq = np.array([1., 0., 0.])
+        aiq = np.dot(aiq,self.R2C)
+
         biq = 0.
 
         return aiq, biq
@@ -140,6 +408,8 @@ class feasible_motion_estimator(object):
         ''' right (i.e., positive) boundary of friction cone '''
        
         aiq = self.friction_parameter_dict['aer']
+        aiq = np.dot(aiq,self.R2C)
+
         biq = np.array(self.friction_parameter_dict['ber'])
         
         return aiq, biq
@@ -148,20 +418,23 @@ class feasible_motion_estimator(object):
         ''' left (i.e., negative) boundary of friction cone '''
 
         aiq = self.friction_parameter_dict['ael']
+        aiq = np.dot(aiq,self.R2C)
+
         biq = np.array(self.friction_parameter_dict['bel'])
      
         return aiq, biq
 
-    def torque_single_pivot_constant_angle_constraint(self,contact_index):
+    def torque_single_pivot_constant_angle_constraint(self):
         ''' 0 net torque w/respect to single external contact '''
+        contact_index = self.single_pivot_contact_index
 
         contact_vertex = self.vertex_array[:,contact_index]
         r_moment_arm = np.array([self.ee_pose_homog[0,3]-contact_vertex[0],self.ee_pose_homog[1,3]-contact_vertex[1]])
 
-        aiq = np.array([[ r_moment_arm[0],-r_moment_arm[1], 1.0],
-                        [-r_moment_arm[0], r_moment_arm[1],-1.0]])
+        aiq = np.array([ r_moment_arm[0],-r_moment_arm[1], 1.])
+        aiq = np.dot(aiq,self.R2C)
 
-        biq = np.array([0.0,0.0])
+        biq = 0.
 
         return aiq, biq
 
@@ -255,6 +528,7 @@ if __name__ == '__main__':
 
 
         motion_estimator.load_system_state(system_state)
+        motion_estimator.check_motion_directions()
         
         # if len(contact_indices)==1:
         #     contact_index = contact_indices[0]
